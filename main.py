2f734fb0ca5b06d34921e5e74ca8270f9fce61ad
Haseeb Mirza <haseemirza123@gmail.com>
2026-05-29 11:45:17 +0500

    fix CORS for Vercel frontend

--- main.py
+++ main.py
@@ -16,7 +16,10 @@ app = FastAPI(
 # CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://localhost:3000",
+        "https://leaf-scan-5mbv3w2f9-hasee1234s-projects.vercel.app"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
